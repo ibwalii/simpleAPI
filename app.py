97cfab67f913326141c8868da437e281d4541d53
Ibrahim Bakari <ibrahimwali4u@gmail.com>
2023-05-07 12:38:36 +0100

    app

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello </h1>'
 
+@app.route('/<name>')
+def print_name(name):
+    return '<h1>Hi, {}</h1>'.format(name)
+
 if __name__ == "__main__":
     app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
